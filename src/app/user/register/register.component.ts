d4d154ff770885b01c925153bafe2df4754140ea
zak <zakaria@webartigiani.it>
2022-06-27 17:15:13 +0200

    added register forms and validation for the form

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+
+  email = new FormControl('',[
+    Validators.required,
+    Validators.email
+  ])
+
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ])
+
+  password = new FormControl('',[
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+
+  confirm_password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/)
+  ])
+
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+
+  showAlert = false
+  alertMsg = 'Please wait! your account is being created'
+  alertColor = 'blue'
+
+  register() {
+    this.showAlert = true
+    this.alertMsg ='Please wait! your account is being created'
+    this.alertColor = 'blue'
+  }
+
+}
